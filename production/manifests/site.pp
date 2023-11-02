6608b82f74418c30aeb2ad8c466729d043418093
root <root@ip-172-31-11-10.eu-west-3.compute.internal>
2023-11-02 06:59:40 +0000

    minor

--- production/manifests/site.pp
+++ production/manifests/site.pp
@@ -24,11 +24,11 @@
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-#node default {
+node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-#}
+}
 
 node 'ip-172-31-12-81.eu-west-3.compute.internal' {   # Ubuntu
   include apache
